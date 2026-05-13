8941c4f72194bb05857b7d773ad84236de1875ba
dev-nirob1 <dev.nirob1@gmail.com>
2026-05-13 21:45:44 +0600

    get api of user

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -2,14 +2,14 @@ import clientPromise from "@/lib/mongodb"
 import { NextResponse } from "next/server"
 import bcrypt from 'bcrypt'
 
-export const GET = async()=> {
-    try{
+export const GET = async () => {
+    try {
         const client = await clientPromise
         const db = client.db()
         const users = await db.collection('users').find({}).toArray()
-        return NextResponse.json({message: 'Users fetched successfully', users}, {status: 200})
-    }   
-    catch(error){
+        return NextResponse.json({ message: 'Users fetched successfully', users }, { status: 200 })
+    }
+    catch (error) {
         console.error('Error fetching users:', error)
         return NextResponse.json({ message: 'Internal server error' }, { status: 500 })
     }
